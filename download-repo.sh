e776708ba29a507fba7b4fdac1c11a7794182960
azurata <git@azurata.me>
2023-08-28 12:09:53 +0900

    feat: チームのリポジトリをダウンロードする処理を別ファイルへ移動

---
+++ download-repo.sh
@@ -0,0 +1,8 @@
+cd RasPike/sdk/workspace
+
+echo "EN: Use GitHub Personal Access Token for Login to GitHub"
+echo "JA: GitHubへのログインにはGitHub Personal Access Tokenを使用してください"
+
+git clone https://github.com/etrobo-fun/etrobo2023_shiokara-z.git shiokara-z
+
+cd shiokara-z
--- install-second.sh
+++ install-second.sh
@@ -8,10 +8,3 @@ mkdir work
 cd work
 
 git clone https://github.com/ETrobocon/RasPike.git
-cd RasPike/sdk/workspace
-
-echo "Use GitHub Personal Access Token for Login to GitHub"
-
-git clone https://github.com/etrobo-fun/etrobo2023_shiokara-z.git shiokara-z
-
-cd shiokara-z
